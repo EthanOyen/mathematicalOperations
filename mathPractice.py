ca063aad626f9e28ffe698fc8c5bb401b8a13b37
EthanOyen <55327075+EthanOyen@users.noreply.github.com>
2019-09-28 12:23:56 -0500

    Initial commit

---
+++ mathPractice.py
@@ -0,0 +1,72 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+#addition
+print(1 + 2)
+a = 2
+b = 14
+print(a + b)
+
+#subtraction
+print(1-2)
+a = 2
+b = 13
+print(a - b)
+
+#multiplication
+print(3*8)
+a = 4
+b = 17
+print(a * b)
+
+#division
+print(8/4)
+a = 18
+b = 9
+print(a / b)
+
+#double division
+print(11 // 4)
+a = 20
+b = 17
+print(a // b)
+
+#modulo
+print(33 % 10)
+a = 41
+b = 17
+print(a % b)
+
+#operators on strings
+#addition
+a = "apples:"
+redApples = 47
+greenApples = 63
+print(a + str(redApples + greenApples))
+
+#subtraction: error
+a = "hellpo wodrld"
+b = "    p    d   "
+print(a - b)
+
+#multiplication
+a = "hello"
+b = "world"
+print(a * b)
+
+#division
+a = "world"
+b = "hello"
+print(a / b)
+
+#double division
+a = "double"
+b = "division"
+print(a // b)
+
+#modulo
+a = "hello"
+b = "world"
+print(a % b)
