2b1f96b0423c0a9e592605e7719f1cd6b055f56a
EthanOyen <55327075+EthanOyen@users.noreply.github.com>
2019-10-05 10:45:14 -0500

    completed debug

--- mathOperatorsDebug.py
+++ mathOperatorsDebug.py
@@ -3,12 +3,15 @@ Created on Oct 5, 2019
 
 @author: ITAUser
 '''
-'''
-40 * 0 = -1     
-“Hello 1 2 and” + 3    
-6 _5 _1= 31    
-print(10 + “/” + 2) = ?        
-print(“LOL” * 3) ?        
-Print( “1” + “2” + “3”)    
-print(10 _ 2 = 0)
-'''
+40 * 0 = -1     #should be zero
+"Hello 1 2 and" + 3    #can't add string and int
+
+#6 _5 _1= 31    
+6*5+1=31
+
+print(10 + "/" + 2) = ?        #can't add string and int
+print("LOL" * 3) ?        #works, only operation on strings that works is multiplication
+print( "1" + "2" + "3")    #works, will print "123"
+
+#print(10 _ 2 = 0)
+print(10 % 2 = 0)
